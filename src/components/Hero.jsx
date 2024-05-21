750c0837ad7e66aa6939d09ab1e0ba2a54f297a9
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-21 12:08:00 -0400

    renamed Home.jsx with Hero.jsx, added Container and Button from react-bootstrap, and LinkContainer from react-router-bootstrap

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,25 @@
-function Home() {
+import {Container, Button} from 'react-bootstrap';
+import "../index.css";
+import { LinkContainer } from 'react-router-bootstrap';
+
+
+const Hero = () => {
   return (
-    <div className="text-black">
-    <div className="hero-image">
+    <Container>
+      <div className="hero-image">
       <div className="hero-text px-5 bg-light" >
         <h1>¡Bienvenidos a LexaViva!</h1>
         <p> &quot;Dive into Spanish Mastery: Elevate Your Vocabulary Journey with LexaViva.&quot;</p>
-          <p><a className="btn btn-success btn-lg" href="/study" role="button">Begin Your Journey</a></p>
+        <LinkContainer to='/study'>
+          <Button className="btn btn-success btn-lg" role="button">Begin Your Journey</Button>
+        </LinkContainer>
     
   
       <p>&quot;To have another language is to possess a second soul.&quot;<br /><br /> - <a href="https://en.wikipedia.org/wiki/Charlemagne">Charlemagne</a></p>
     </div>
     
     </div> 
-    </div>
+    </Container>
   )
 }
-
-export default Home
+export default Hero;
