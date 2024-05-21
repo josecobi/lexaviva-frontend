5675165e10cd0a482cea58cc0cfde1994f3c2ced
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-21 11:54:24 -0400

    Added redux for user authentication, Outlet, and private routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import './index.css'
-import { Route, Routes } from "react-router-dom";
-import Nav from './components/Nav'
+import { Outlet } from 'react-router-dom';
+// import './index.css'
+import { Container } from 'react-bootstrap';
+import Header from './components/Header'
 import Footer from './components/Footer';
-import Home from './pages/Home';
-import About from './pages/About';
-import Study from './pages/Study';
-import MyTopics from './pages/MyTopics';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import toast, {Toaster}  from 'react-hot-toast';
+import {Toaster}  from 'react-hot-toast';
 
 function App() {
   
-  return (
+  return (    
     <div className="App">
-      <Nav />
       <Toaster position='bottom-right' toastOptions={{duration : 2000}} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/study" element={<Study />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/MyTopics" element={<MyTopics />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Footer />     
+      <Header />
+      <Container className="my-2">
+        <Outlet />
+      </Container>
+
+
+      
+
+      <Footer /> 
+  
     </div>
   )
 }
 
-export default App
+export default App;
