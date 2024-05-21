57eb9920f303c86a3dfd22272e8bfd09bcb396c2
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-21 16:19:34 -0400

    fixed USERS_URL

--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -1,6 +1,6 @@
 import { apiSlice } from "./apiSlice";
 // Define the base URL for the users API
-const USERS_URL = "https://lexaviva.vercel.app/api/users";
+const USERS_URL = "https://lexaviva-backend.vercel.app/api/users";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
