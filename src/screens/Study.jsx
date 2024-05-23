9646a21b77321a5efd96801c7ae6ce53019553f8
josecobi <58313777+josecobi@users.noreply.github.com>
2024-05-23 14:18:43 -0400

    Justify content to center in the Study screen.

--- src/screens/Study.jsx
+++ src/screens/Study.jsx
@@ -2,7 +2,7 @@ import SelectTopic from '../components/study/SelectTopic'
 function Study() {
 
   return (
-    <div className="container mt-2">
+    <div className=" row justify-content-center mt-2">
         <h2>Vocabulary Cards</h2>
         <p>Study with flashcards to help you learn and retain information. Hover the card with your mouse to reveal the meaning.</p> 
         <hr />     
