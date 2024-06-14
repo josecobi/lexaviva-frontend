920163374d92d4eafe01ce82ca3acc930b9f7974
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-14 13:14:19 -0400

    Created grid to display image results from the image search.

---
+++ src/components/topics/IllustrationGrid.jsx
@@ -0,0 +1,45 @@
+import Illustration from './Illustration';
+import propTypes from "prop-types";
+import Loader from '../Loader';
+import {selectIllustration, deSelectIllustration} from '../../slices/illustrationSlice';
+import {useDispatch, useSelector} from 'react-redux';
+
+
+
+
+
+function IllustrationGrid({imgResults}){
+    const dispatch = useDispatch();
+    const {selectedIllustration} = useSelector((state) => state.illustration);
+    // if there are no image results, display the loader
+    if(!imgResults) return <Loader />
+
+    const handleIllustrationClick = (src, author) => {
+        // Check if the illustration is already selected
+        if(selectedIllustration.src === src && selectedIllustration.author === author) {
+            // If it is, deselect it
+            console.log(`img from ${author} deselected`)
+            return dispatch(deSelectIllustration());
+        }
+        // If it is not, select it
+        console.log(`img from ${author} selected`)
+        dispatch(selectIllustration({src, author}));
+    }
+    return(
+        <div className="grid">
+            {   imgResults.map((img) => {           
+                    const imgId = img.id.toString();
+                    const src = img.image.source.url;
+                    const author = img.author.name;
+                    return <Illustration onClick={() => (handleIllustrationClick(src, author))} key={imgId} imgId={imgId} src={src} author={author} />
+                }           
+            )}   
+
+        </div>
+    )
+
+}
+IllustrationGrid.propTypes = {
+    imgResults: propTypes.array,
+}
+export default IllustrationGrid;
