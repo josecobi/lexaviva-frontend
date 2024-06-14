a54f7549fbbc750bad3e952893eef45dd53ede07
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-14 13:06:29 -0400

    Restructured the MyTopics layout.

--- src/screens/MyTopics.jsx
+++ src/screens/MyTopics.jsx
@@ -1,35 +1,67 @@
-import ActionCard from "../components/topics/ActionCard"
-import { Container, Row, Col } from 'react-bootstrap'
+// import ActionCard from "../components/topics/ActionCard"
+// import { Container, Row, Col } from 'react-bootstrap'
+import { Card, Col, Row, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 
 
 
 const MyTopics = () => {
-    
     return(
-        <>
-        <Container fluid className="mt-5">
-      {/* <Row className="justify-content-md-center text-center g-5"> */}
-      <Row className="justify-content-center g-5">
+        <Container className="mt-5">
+        <Row className="g-4 myTopicsRow justify-content-center">
+        <Col xs={12} md={4} className="myTopicsCol ">
+            <LinkContainer to="/EditTopics" className="clickableElement">
+                <Card className="myTopicCards p-2">
+                    <Card.Img variant="top" src="src\assets\editable.svg" className="myTopicsCardImg"/>
+                    <Card.Body>
+                    <Card.Title>Edit Topics</Card.Title>
+                    <Card.Text>
+                    Edit, add, and delete words from your topics with ease. You can also change their illustrations.
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+            </LinkContainer>
+        </Col>
+        <Col xs={12} md={4} className="myTopicsCol">      
+            <LinkContainer to="/NewTopic" className="clickableElement">
+            <Card className="myTopicCards p-2">
+                <Card.Img variant="top" src="src/assets/create.svg" className="myTopicsCardImg" />
+                <Card.Body>
+                <Card.Title>Create New Topic</Card.Title>
+                <Card.Text>
+                Create topics adapted to your skill level.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            </LinkContainer>
+        </Col>
+      
+    </Row>
+    </Container>
+    //     <>
+    //     <Container fluid className="mt-5">
+    //   <Row className="justify-content-center g-5">
 
-        <Col xs={10} md={3} >
-        <ActionCard 
-            title='Edit Topics' 
-            text='Edit, add, and delete words from your topics with ease.' 
-            action='Edit' link='/EditTopics' 
-            icon={["fa-solid", "fa-pen-to-square"]}
-        />
-        </Col>
-        <Col xs={10} md={3}>
-        <ActionCard 
-            title='Create New Topic' 
-            text='Create topics adapted to your skill level. ' 
-            action='Create' link='/NewTopic' 
-            icon={["fa-solid", "fa-circle-plus"]}
-        />
-        </Col>
-        </Row>
-        </Container>
-        </>
+    //     <Col xs={10} md={3} >
+    //     <ActionCard 
+    //         title='Edit Topics' 
+    //         text='Edit, add, and delete words from your topics with ease.' 
+    //         action='Edit' link='/EditTopics' 
+    //         icon={["fa-solid", "fa-pen-to-square"]}
+    //     />
+    //     </Col>
+    //     <Col xs={10} md={3}>
+    //     <ActionCard 
+    //         title='Create New Topic' 
+    //         text='Create topics adapted to your skill level. ' 
+    //         action='Create' link='/NewTopic' 
+    //         icon={["fa-solid", "fa-circle-plus"]}
+    //     />
+    //     </Col>
+    //     </Row>
+    //     </Container>
+    //     </>
     )    
 }
 
