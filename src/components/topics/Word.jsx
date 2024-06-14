2c3bf1f0d22652793afbeb3646f759250ef94fcf
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-14 13:11:04 -0400

    Added search images feature.

--- src/components/topics/Word.jsx
+++ src/components/topics/Word.jsx
@@ -1,75 +1,104 @@
 import propTypes from 'prop-types';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
-import {FaTrashAlt} from 'react-icons/fa';
-
-
-
-
 import { useSelector } from 'react-redux';
+import SearchImages from './SearchImages';
+import { useEffect, useState } from 'react';
+
 
 // This component is used to display the words in the table
-function Word({document, updateData, fetchedData}) {
+function Word({wordData, setFetchedData, fetchedData}) {
   const {userInfo} = useSelector((state) => state.auth)
   const user_id = userInfo._id
+  const {selectedIllustration} = useSelector((state) => state.illustration);
+  const [document, setDocument] = useState({_id: wordData._id,
+    topic: wordData.topic,
+    english_word: wordData.english_word,
+    word: wordData.word,
+    attribution: wordData.attribution,
+    imgUrl: wordData.imgUrl,
+  });
+
+  const fallbackImgUrl = 'https://via.placeholder.com/150';
+
+ // Update the document's fields when changes are made
+ useEffect(() => {
+  setDocument((prevDoc) => ({ ...prevDoc,  imgUrl: selectedIllustration.src, attribution: selectedIllustration.author}));
+}, [selectedIllustration.src, selectedIllustration.author]);
+
+const handleInputChange = (e) => {
+  const { name, value } = e.target;
+  setDocument((prevDoc) => ({
+    ...prevDoc,
+    [name]: value
+  }));
+};
+
   return (
     <>
       
-      <form data-bs-theme="light" onSubmit={(event) => handleSaveChanges(event, updateData, fetchedData, user_id)} className="row g-3">   
+      <form data-bs-theme="light" onSubmit={(event) => handleSaveChanges(event, setFetchedData, fetchedData, user_id)} className="row justify-content-center g-3">   
+      <div className="col-2 justify-content-center mb-3">
+            <p>Illustration preview</p>
+              <img className="thumbnail" src={document.imgUrl || fallbackImgUrl} alt='Illustration thumbnail'></img>
+          </div>
+          <div className="col-md-3 col-sm-4">
+              <label htmlFor="topic" className="form-label">Topic</label>
+              <input name="topic"  id="topic" className="form-control" type="text" required value={document.topic}
+            onChange={handleInputChange}></input>
+          </div>
+          <div className="col-md-3 col-sm-4 col-xsm-4">
+              <label htmlFor="english_word" className="form-label">English Word</label>
+              <input name="english_word" id="english_word" className="form-control" type="text" required value={document.english_word}
+            onChange={handleInputChange}></input>
+          </div>
+          <div className="col-md-3 col-sm-4" >
+          <label htmlFor="word" className="form-label">Word</label>
+              <input name="word" id="word" className="form-control" type="text" value={document.word}
+            onChange={handleInputChange}></input>
+          </div>  
+          {/* <div className="col-md-3 col-sm-4">
+              <label htmlFor="attribution" className="form-label">Attribution</label>
+              <input name="attribution" id="attribution" className="form-control" type="text" value={document.attribution}
+            onChange={handleInputChange}></input>
+          </div>             
+          <div className="col-12">
+              <label htmlFor="imgUrl" className="form-label">Image URL</label>
+              <input name="imgUrl" id="imgUrl" className="form-control" type="text" value={document.imgUrl}
+            onChange={handleInputChange}></input>
+          </div> */}
+          <input name="id" className="form-control" type="hidden" defaultValue={document._id}></input>                
+          
+          <div className="container fluid my-3">
+              <Button onClick={(event) => handleRemoveWord(event, fetchedData, setFetchedData)} value={document._id} className="btn btn-danger me-3">Remove</Button>     
+              <button  type="submit" className="btn btn-success">Save</button>
+          </div>
+          
 
-                <div className="col-md-3 col-sm-4">
-                    <label htmlFor="topic" className="form-label">Topic</label>
-                    <input name="topic"  id="topic" className="form-control" type="text" required defaultValue={document.topic}></input>
-                </div>
-                <div className="col-md-3 col-sm-4 col-xsm-4">
-                    <label htmlFor="english_word" className="form-label">English Word</label>
-                    <input name="english_word" id="english_word" className="form-control" type="text" required defaultValue={document.english_word}></input>
-                </div>
-                <div className="col-md-3 col-sm-4" >
-                <label htmlFor="word" className="form-label">Word</label>
-                    <input name="word" id="word" className="form-control" type="text" defaultValue={document.word}></input>
-                </div>  
-                <div className="col-md-3 col-sm-4">
-                    <label htmlFor="attribution" className="form-label">Attribution</label>
-                    <input name="attribution" id="attribution" className="form-control" type="text" defaultValue={document.attribution}></input>
-                </div>             
-                <div className="col-12 ">
-                    <label htmlFor="imgUrl" className="form-label">Image URL</label>
-                    <input name="imgUrl" id="imgUrl" className="form-control" type="text" defaultValue={document.imgUrl}></input>
-                </div>
-                
-                <input name="id" className="form-control" type="hidden" defaultValue={document._id}></input>                
-                
-                <div>
-                    <Button onClick={(event) => handleRemoveWord(event, fetchedData, updateData)} value={document._id} className="btn btn-danger"><FaTrashAlt/>Remove</Button>
-                </div>
-                <div>
-                    <button  type="submit" className="btn btn-success">Save</button>
-                </div>
-               
+      </form>
+      <SearchImages />
 
-            </form>
     </>
   )
   // function used to update the word
-  async function handleSaveChanges(event, updateData, fetchedData, user_id) {
+  async function handleSaveChanges(event, setFetchedData, fetchedData, user_id) {
     event.preventDefault();
       
       try{
-        const formdata = {
+        const formData = {
           _id: event.target.id.value,
           word: event.target.word.value,
-          imgUrl: event.target.imgUrl.value,
-          attribution: event.target.attribution.value,
+          imgUrl: selectedIllustration.src,
+          attribution: selectedIllustration.author,
           english_word: event.target.english_word.value,
           topic: event.target.topic.value,
           user_id: user_id
-
       }
+
       console.log("event id:", event.target.id.value);
-      console.log("save this data:", formdata);
+      console.log("save this data:", formData);
         // update the word
-         const response = await axios.put(`api/words/update/${event.target.id.value}`, formdata);
+         const response = await axios.put(`api/words/update/${event.target.id.value}`, formData);
         
         // log the updated word
         console.log("response data should be the updated word: ", response.data);
@@ -84,7 +113,7 @@ function Word({document, updateData, fetchedData}) {
         newData[indexOfModifiedWord] = response.data;
 
         // lift up the state so the word is updated in the list
-        updateData(newData);
+        setFetchedData(newData);
       } 
       catch(error){
         console.error("Error:", error.message);
@@ -92,7 +121,7 @@ function Word({document, updateData, fetchedData}) {
    
   }
   // function used to remove the word
-  async function handleRemoveWord(event, fetchedData, updateData) {
+  async function handleRemoveWord(event, fetchedData, setFetchedData) {
     try {
       console.log("event id:", event.target.value);
       // alert the user to confirm the deletion
@@ -104,7 +133,7 @@ function Word({document, updateData, fetchedData}) {
         
         // lift up the state so the word is removed from the list 
         const wordsNotDeleted = fetchedData.filter(word => word && word._id !== event.target.value);
-        updateData(wordsNotDeleted);
+        setFetchedData(wordsNotDeleted);
       }    
    
     }
@@ -117,9 +146,9 @@ function Word({document, updateData, fetchedData}) {
 }
 // define the prop types
 Word.propTypes = {
- document: propTypes.object.isRequired,
+ wordData: propTypes.object.isRequired,
   selectedTopic: propTypes.string.isRequired,
-  updateData: propTypes.func.isRequired,
+  setFetchedData: propTypes.func.isRequired,
   fetchedData: propTypes.array.isRequired
 }
 export default Word
