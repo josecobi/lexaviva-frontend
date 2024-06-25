9a81fdd196c4c59f448644bb11d470f30a4b9306
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-25 11:02:29 -0400

    Changed classes names and added logic for responsiveness

--- src/screens/MyTopics.jsx
+++ src/screens/MyTopics.jsx
@@ -1,5 +1,3 @@
-// import ActionCard from "../components/topics/ActionCard"
-// import { Container, Row, Col } from 'react-bootstrap'
 import { Card, Col, Row, Container } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
@@ -9,11 +7,11 @@ import { LinkContainer } from 'react-router-bootstrap';
 const MyTopics = () => {
     return(
         <Container className="mt-5">
-        <Row className="g-4 myTopicsRow justify-content-center">
-        <Col xs={12} md={4} className="myTopicsCol ">
+        <Row className="g-4 infoCardsRow justify-content-center">
+        <Col xs={12} md={4} className="infoCardsCol">
             <LinkContainer to="/EditTopics" className="clickableElement">
-                <Card className="myTopicCards p-2">
-                    <Card.Img variant="top" src="/assets/editable.svg" className="myTopicsCardImg"/>
+                <Card className="infoCards p-2">
+                    <Card.Img variant="top" src="./assets/editable.svg" className="infoCardImg"/>
                     <Card.Body>
                     <Card.Title>Edit Topics</Card.Title>
                     <Card.Text>
@@ -23,10 +21,10 @@ const MyTopics = () => {
                 </Card>
             </LinkContainer>
         </Col>
-        <Col xs={12} md={4} className="myTopicsCol">      
+        <Col xs={12} md={4} className="infoCardsCol">      
             <LinkContainer to="/NewTopic" className="clickableElement">
-            <Card className="myTopicCards p-2">
-                <Card.Img variant="top" src="/assets/create.svg" className="myTopicsCardImg" />
+            <Card className="infoCards p-2">
+                <Card.Img variant="top" src="./assets/create.svg" className="infoCardImg" />
                 <Card.Body>
                 <Card.Title>Create New Topic</Card.Title>
                 <Card.Text>
@@ -39,29 +37,6 @@ const MyTopics = () => {
       
     </Row>
     </Container>
-    //     <>
-    //     <Container fluid className="mt-5">
-    //   <Row className="justify-content-center g-5">
-
-    //     <Col xs={10} md={3} >
-    //     <ActionCard 
-    //         title='Edit Topics' 
-    //         text='Edit, add, and delete words from your topics with ease.' 
-    //         action='Edit' link='/EditTopics' 
-    //         icon={["fa-solid", "fa-pen-to-square"]}
-    //     />
-    //     </Col>
-    //     <Col xs={10} md={3}>
-    //     <ActionCard 
-    //         title='Create New Topic' 
-    //         text='Create topics adapted to your skill level. ' 
-    //         action='Create' link='/NewTopic' 
-    //         icon={["fa-solid", "fa-circle-plus"]}
-    //     />
-    //     </Col>
-    //     </Row>
-    //     </Container>
-    //     </>
     )    
 }
 
--- src/screens/NewTopic.jsx
+++ src/screens/NewTopic.jsx
@@ -1,45 +1,56 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState, useEffect } from 'react';
-import NewWord from '../components/topics/NewWord';
 import NewTopicForm from '../components/topics/NewTopicForm'
 import axios from 'axios';
 import Table from "../components/topics/Table";
-import toast from 'react-hot-toast';
 import Loader from '../components/Loader';
+import ModalTopicAlreadyExists from '../components/topics/ModalTopicAlreadyExists';
+import NewWord from '../components/topics/NewWord';
+import { Container } from 'react-bootstrap';
+import { setTopic } from '../slices/topicSlice';
+
+
+
 
 // Component to add a new word
 function NewTopic() {
   const {userInfo} = useSelector((state) => state.auth)
   const user_id = userInfo._id
   const [newTopicName, setNewTopicName] = useState("");
-  const [isFirstWordSaved, setIsFirstWordSaved] = useState(false);
   const [fetchedData, setFetchedData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [topicExists, setTopicExists] = useState(false);
+  const [modalShow, setModalShow] = useState(false);
+  const [isFirstWordSaved, setIsFirstWordSaved] = useState(false);
+  const selectedTopic = useSelector((state) => state.topic.selectedTopic);
+  const dispatch = useDispatch();
 
-
-useEffect(() => {
-    const fetchData = async () => {
+  useEffect(() => {
+    const getTopicsData = async () => {
       setIsLoading(true);
       
-
       try {
         // Fetch data from a REST API. Each word has a topic field in the database. If the response contains at least one word, the topic already exists
-        const response = await axios.get(`/api/words/bytopic?selectedTopic=${newTopicName}&user_id=${user_id}`);
-        if (response.data.length > 1) {
-            setNewTopicName("");
-            // send error message and toast letting user know that topic exists and to choose another or edit topics in the table component
-            console.log("Topic exists, choose another or edit the existing topic in the 'My Topics' section");
-            toast.error("Topic exists, choose another or edit the existing topic in the 'My Topics' section", {duration: 5000});
-            
-            // setFetchedData(response.data)          
-
-            
-            // console.log("set fetched data to response data and set first word to 'true'")
-        }
-        else if (response.data.length === 1) {
+        const response = await axios.get(`/api/words/topicsByUser?selectedTopic=${newTopicName}&user_id=${user_id}`);
+        if (response.data.length > 0){
+          console.log("Topic exists : ", response.data)
+          setTopicExists(true);
+          dispatch(setTopic(newTopicName));
+          setModalShow(true);
           setIsFirstWordSaved(true);
-          console.log("Response data length === 1");
-        }
+          const words = await axios.get(`/api/words/byTopic?selectedTopic=${newTopicName}&user_id=${user_id}`);
+          setFetchedData(words.data);
+          
+          console.log("words.data: ", words.data);
+        } 
+        else {
+          setTopicExists(false);
+          console.log("response: ", response.data.length)
+          
+        } 
+        console.log("response.data: ", response.data)
+      
+      
         
       } catch (error) {
         //Handle error if the request fails
@@ -50,36 +61,46 @@ useEffect(() => {
     
     // Call the fetchData function
     if (newTopicName) {
-      fetchData();
+      getTopicsData();
     }
-  }, [newTopicName, user_id, fetchedData, isFirstWordSaved]);
+  }, [newTopicName, user_id, dispatch]);
 
-    
+    //Handle hide. The topic that already exists is displayed in the table behind the modal
+    const handleHide = () => {
+      setModalShow(false);
+      dispatch(setTopic(newTopicName));//so the name of the topic is displayed in the table
+      setNewTopicName(""); //reset the topic name
+      setTopicExists(false);     
+    }
+
+    // handle click event to set the new topic name
+    const handleClickNewTopicName = () => {
+      setModalShow(false);
+      setNewTopicName("")
+      setTopicExists(false);
+      setIsFirstWordSaved(false);//so the new word form is displayed
+    }
+
   return (
     <>
-   {isLoading ? (
-            <Loader /> // Or replace with your own loading component
-        ) : (
-            <>
-                {!isFirstWordSaved && newTopicName && <NewWord selectedTopic={newTopicName} setFetchedData={setFetchedData} fetchedData={fetchedData}/> }
-                {!newTopicName && <NewTopicForm setNewTopicName={setNewTopicName} />}  
-                {isFirstWordSaved && <Table fetchedData={fetchedData} selectedTopic={newTopicName} setFetchedData={setFetchedData}/>}
-            </>
+        {isLoading ? (<Loader />) : (
+            <Container>
+              {/* show modal if the topic already exists */}
+              {modalShow && (<ModalTopicAlreadyExists topic={selectedTopic} show={modalShow} onHide={handleHide} handleClickNewTopicName={handleClickNewTopicName}/>)}
+
+              {/* if the topic does not exist, show the new topic form */}
+              {!topicExists && !newTopicName && !isFirstWordSaved && (<NewTopicForm setNewTopicName={setNewTopicName}/>) }
+
+              {/* if the user entered a new topic name, if the topic does not exist in the DB, show the new word form */}  
+              {newTopicName && !topicExists && <NewWord selectedTopic={newTopicName} setFetchedData={setFetchedData} fetchedData={fetchedData} setIsFirstWordSaved={setIsFirstWordSaved} setTopicExists={setTopicExists}/> }
+
+              {/* if the topic exists because it already has a word in it, show the table with the words */}
+              {isFirstWordSaved && <Table fetchedData={fetchedData} selectedTopic={selectedTopic} setFetchedData={setFetchedData}/>}
+            </Container>
         )}
-
-   {/* {!isFirstWordSaved && newTopicName &&  <NewWord selectedTopic={newTopicName} setFetchedData={setFetchedData} fetchedData={fetchedData}/> }
-   
-  {!newTopicName && <NewTopicForm setNewTopicName={setNewTopicName} />}   */}
-   
-
-    
-
-    {/* {isFirstWordSaved && <Table fetchedData={fetchedData} selectedTopic={newTopicName} setFetchedData={setFetchedData}/>} */}
-
-
     </>
   )
 }
 
 
-export default NewTopic
+export default NewTopic;
