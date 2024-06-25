38e621eb548cd7b24486bb846e7db2339bac4085
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-25 09:57:30 -0400

    Added imgId to the arguments to keep track which image is selected and apply styles to it

--- src/components/topics/IllustrationGrid.jsx
+++ src/components/topics/IllustrationGrid.jsx
@@ -14,16 +14,18 @@ function IllustrationGrid({imgResults}){
     // if there are no image results, display the loader
     if(!imgResults) return <Loader />
 
-    const handleIllustrationClick = (src, author) => {
+    const handleIllustrationClick = (src, author, id) => {
         // Check if the illustration is already selected
-        if(selectedIllustration.src === src && selectedIllustration.author === author) {
+        if(selectedIllustration.src === src && selectedIllustration.author === author && selectedIllustration.id === id) {
             // If it is, deselect it
             console.log(`img from ${author} deselected`)
             return dispatch(deSelectIllustration());
         }
         // If it is not, select it
-        console.log(`img from ${author} selected`)
-        dispatch(selectIllustration({src, author}));
+        console.log(`img from ${author} selected with id ${id}`)
+        dispatch(selectIllustration({src, author, id}));
+        console.log("Selectedillustration img state: ", selectedIllustration)
+
     }
     return(
         <div className="grid">
@@ -31,7 +33,7 @@ function IllustrationGrid({imgResults}){
                     const imgId = img.id.toString();
                     const src = img.image.source.url;
                     const author = img.author.name;
-                    return <Illustration onClick={() => (handleIllustrationClick(src, author))} key={imgId} imgId={imgId} src={src} author={author} />
+                    return <Illustration onClick={() => (handleIllustrationClick(src, author, imgId))} key={imgId} imgId={imgId} src={src} author={author} />
                 }           
             )}   
 
