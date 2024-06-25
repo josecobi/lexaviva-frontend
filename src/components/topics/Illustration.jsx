fd6c399583f9b50f9a1a0e965a38d813a467fe66
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-25 09:55:56 -0400

    added classes to style selected images

--- src/components/topics/Illustration.jsx
+++ src/components/topics/Illustration.jsx
@@ -1,13 +1,13 @@
 import propTypes from 'prop-types'
 import { Figure } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
-export default function Illustration({imgId, src, author, onClick}){
-
-    // console.log("img source: ", src);
+export default function Illustration({ imgId, src, author, onClick}){
+    const { selectedIllustration } = useSelector((state) => state.illustration);
+    const isSelected = selectedIllustration.id === imgId;
 
     return(
-        <Figure onClick={onClick} className="imgContainer">
-            <Figure.Image loading="lazy" rounded src={src} id={imgId}></Figure.Image>
+        <Figure onClick={onClick} className={`imgContainer img-thumbnail clickableElement ${isSelected ? 'border border-2 border-dark' : ''}`}>            <Figure.Image loading="lazy" rounded src={src} id={imgId}></Figure.Image>
             <Figure.Caption>By {author} from Freepik.com</Figure.Caption>
         </Figure>
     )
