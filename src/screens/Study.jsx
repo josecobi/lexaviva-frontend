3e2e7eb6ded5606c3b768d8c4a83640d9c82375f
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-25 11:03:14 -0400

    Replaced div for Row component and increased margin

--- src/screens/Study.jsx
+++ src/screens/Study.jsx
@@ -1,14 +1,17 @@
+import { Col, Row } from 'react-bootstrap'
 import SelectTopic from '../components/study/SelectTopic'
 function Study() {
 
   return (
-    <div className="row justify-content-center mt-2">
+    <Row className="row justify-content-center mt-5">
+      <Col xs={12} md={8}>
         <h2>Vocabulary Cards</h2>
         <p>Study with flashcards to help you learn and retain information. Hover the card with your mouse to reveal the meaning.</p> 
-        <hr />     
+        <hr />  
+      </Col>   
         <SelectTopic />
    
-    </div>
+    </Row>
   )
 }
 
