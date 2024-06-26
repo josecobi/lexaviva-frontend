41d2d2d580e788079959213c5cb44a68e56d1565
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-26 06:31:40 -0400

    Fixed attribution. Added anchor tag

--- src/components/study/StudyCard.jsx
+++ src/components/study/StudyCard.jsx
@@ -13,7 +13,7 @@ function StudyCard({word, imgUrl, attribution, english_word}) {
             <div className="flip-card-back">           
               <img className="card-image" src={imgUrl} alt={altText}/>
               <h2>{word}</h2>
-              <figcaption className="figure-caption">{attribution}.</figcaption>
+              <figcaption className="figure-caption">By {attribution} from <a href="https://www.freepik.com/" target="_blank" rel="noopener noreferrer">Freepik.com</a>.</figcaption>
             </div>
           </div>
       </div>
