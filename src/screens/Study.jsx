b1f3df41d9911244afdce39ff509b8399d1ad546
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-30 23:23:53 -0400

    Added container to Study component

--- src/screens/Study.jsx
+++ src/screens/Study.jsx
@@ -1,17 +1,18 @@
-import { Col, Row } from 'react-bootstrap'
+import { Container, Col, Row } from 'react-bootstrap'
 import SelectTopic from '../components/study/SelectTopic'
 function Study() {
 
   return (
-    <Row className="row justify-content-center mt-5">
-      <Col xs={12} md={8}>
+    <Container className="study-container mt-2 p-3">
+    <Row className="row mt-3">
+      <Col className="text-center" >
         <h2>Vocabulary Cards</h2>
-        <p>Study with flashcards to help you learn and retain information. Hover the card with your mouse to reveal the meaning.</p> 
-        <hr />  
+        <p>Flip the card to reveal the meaning.</p> 
       </Col>   
-        <SelectTopic />
+      <SelectTopic />
    
     </Row>
+    </Container>
   )
 }
 
