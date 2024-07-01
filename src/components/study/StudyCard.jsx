83c756f23e1a62df5d00efe8e8e0c776ac0faa12
josecobi <58313777+josecobi@users.noreply.github.com>
2024-06-30 23:23:00 -0400

    Changed styles in StudyCard component

--- src/components/study/StudyCard.jsx
+++ src/components/study/StudyCard.jsx
@@ -5,12 +5,13 @@ function StudyCard({word, imgUrl, attribution, english_word}) {
   console.log("word, img, attr, english word: ", word, imgUrl, attribution, english_word);
   const altText = `Image of: ${english_word}`;
   return (
-      <div className="flip-card container mt-2 mb-3" >
+      <div className="flip-card container mt-1 mb-3" >
           <div className="flip-card-inner">
           <div className="flip-card-front p-2 d-flex justify-content-center align-items-center">
   <h1 className="align-self-center">{english_word}</h1>                  
             </div>
-            <div className="flip-card-back">           
+            <div className="flip-card-back"> 
+                     
               <img className="card-image" src={imgUrl} alt={altText}/>
               <h2>{word}</h2>
               <figcaption className="figure-caption">By {attribution} from <a href="https://www.freepik.com/" target="_blank" rel="noopener noreferrer">Freepik.com</a>.</figcaption>
