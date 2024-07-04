fe515e0f6265386cb79249185057185f314b550c
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-04 08:30:54 -0400

    Styled container and title

--- src/screens/Study.jsx
+++ src/screens/Study.jsx
@@ -3,10 +3,10 @@ import SelectTopic from '../components/study/SelectTopic'
 function Study() {
 
   return (
-    <Container className="study-container mt-2 p-3">
-    <Row className="row mt-3">
+    <Container className="screen-container mt-4 pt-3 pb-5">
+    <Row className="row mt-1">
       <Col className="text-center" >
-        <h2>Vocabulary Cards</h2>
+        <p className="pages-title">Vocabulary Cards</p>
         <p>Flip the card to reveal the meaning.</p> 
       </Col>   
       <SelectTopic />
