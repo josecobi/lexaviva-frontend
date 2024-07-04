43d80be9bf2dae778b6e857739da5fa6bf68d35d
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-04 07:35:41 -0400

    Created new svg navigation buttons

---
+++ src/components/icons/ArrowIcons.jsx
@@ -0,0 +1,27 @@
+export const ArrowLeftIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 512 512">
+        <circle cx="256" cy="256" r="256" fill="#FF5858" />
+        <path d="M271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" fill="#FFFFFF" />
+    </svg>
+);
+
+export const ArrowRightIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 512 512">
+        <circle cx="256" cy="256" r="256" fill="#FF5858" />
+        <path d="M241 135c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l87 87-87 87c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0L345 273c9.4-9.4 9.4-24.6 0-33.9L241 135z" fill="#FFFFFF" />
+    </svg>
+);
+
+export const ArrowLeftIconLight = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 512 512">
+        <circle cx="256" cy="256" r="256" fill="#FFFFFF" />
+        <path d="M271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" fill="#FF5858" />
+    </svg>
+);
+
+export const ArrowRightIconLight = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" height="40" width="40" viewBox="0 0 512 512">
+        <circle cx="256" cy="256" r="256" fill="#FFFFFF" />
+        <path d="M241 135c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l87 87-87 87c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0L345 273c9.4-9.4 9.4-24.6 0-33.9L241 135z" fill="#FF5858" />
+    </svg>
+);
