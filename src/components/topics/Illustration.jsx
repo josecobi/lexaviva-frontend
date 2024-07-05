68e607ed3ecf96c64e3dd92f6e866a212db2b97e
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-05 11:33:49 -0400

    Edited attribution text.

--- src/components/topics/Illustration.jsx
+++ src/components/topics/Illustration.jsx
@@ -8,7 +8,7 @@ export default function Illustration({ imgId, src, author, onClick}){
 
     return(
         <Figure onClick={onClick} className={`imgContainer img-thumbnail clickableElement ${isSelected ? 'border border-2 border-dark' : ''}`}>            <Figure.Image loading="lazy" rounded src={src} id={imgId}></Figure.Image>
-            <Figure.Caption>By {author} from Freepik.com</Figure.Caption>
+            <Figure.Caption>By {author} on Freepik.com</Figure.Caption>
         </Figure>
     )
 }
