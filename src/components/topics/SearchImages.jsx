cad1202548147b2c8455b2d3eb56c6756f570eed
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-05 11:38:13 -0400

    removed unnecessary import

--- src/components/topics/SearchImages.jsx
+++ src/components/topics/SearchImages.jsx
@@ -1,4 +1,4 @@
-import {Form, Col, Row, Button} from "react-bootstrap";
+import {Form, Col, Row} from "react-bootstrap";
 import { useState, useEffect } from "react";
 import IllustrationGrid from "./IllustrationGrid";
 import { useSelector } from "react-redux";
