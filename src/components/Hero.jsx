16a57c2244af57328ba5e6dfc099eba768f7eb2b
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-05 11:32:52 -0400

    added new hero image. Splitted the hero section in two responsive columns.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
 import { LinkContainer } from 'react-router-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap'
 const Hero = () => {
   return (
     
-      <div className="hero-section container">
-        <div className="hero-text container p-3">
+      <Container className="hero-section d-flex container">
+
+        <Row className="hero-text justify-content-center align-items-center p-3">
+          <Col sm={12} md={6}>
           <h1 className="hero-title mb-3">Fluent in Spanish, Fluent in Life</h1>
           <h4 className="mb-3">Elevate Your Vocabulary with LexaViva</h4>
           <LinkContainer to='/study'>
             <button className="button-primary mb-3" role="button">Begin Your Journey</button>
           </LinkContainer>
-      </div>
-      <img src="/assets/student-laptop-bed-flipped.avif" alt="hero-image-student-laptop" className="hero-img"></img>
+          </Col>
+          <Col sm={12} md={6}>
+            <img src="/assets/students-hero-img.svg" alt="hero-image-student-laptop" className="hero-img"></img>
+          </Col>
+      </Row>
 
-    </div> 
+    </Container> 
   )
 }
 export default Hero;
