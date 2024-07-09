8365f757f21c3e772dfa1934128c4cc95b970f57
josecobi <58313777+josecobi@users.noreply.github.com>
2024-07-09 11:58:36 -0400

    Added footer and button to card.

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -6,7 +6,6 @@ function Dasboard() {
         <Container className="mt-5">
             <Row className="infoCardsRow justify-content-center g-3">
                 <Col xs={10} md={3} className="infoCardsCol">
-                    <LinkContainer to="/study" className="clickableElement">
                     <Card className="infoCards p-2">
                         <Card.Img variant="top" src="./assets/study.svg" className="infoCardImg"/>
                         <Card.Body>
@@ -15,11 +14,16 @@ function Dasboard() {
                                 Master Vocabulary Effortlessly with Our Interactive Cards!
                             </Card.Text>
                         </Card.Body>
+                        <Card.Footer>
+            <LinkContainer to="/Study" className="clickableElement">
+
+                <button className="button-primary">Study</button>
+            </LinkContainer>
+
+                </Card.Footer>
                     </Card>
-                    </LinkContainer>                  
                 </Col>
                 <Col xs={10} md={3} className="infoCardsCol">
-                    <LinkContainer to="/profile" className="clickableElement">
                     <Card className="infoCards p-2">
                         <Card.Img variant="top" src="./assets/profile.svg" className="infoCardImg"/>
                         <Card.Body>
@@ -28,11 +32,16 @@ function Dasboard() {
                                 View and edit your profile information.
                             </Card.Text>
                         </Card.Body>
+                        <Card.Footer>
+            <LinkContainer to="/Profile" className="clickableElement">
+
+                <button className="button-primary">Profile</button>
+            </LinkContainer>
+
+                </Card.Footer> 
                     </Card>
-                    </LinkContainer>
                 </Col>
                 <Col xs={10} md={3} className="infoCardsCol">
-                    <LinkContainer to="/mytopics" className="clickableElement">
                     <Card className="infoCards p-2">
                         <Card.Img variant="top" src="./assets/topics.svg" className="infoCardImg"/>
                         <Card.Body>
@@ -41,8 +50,14 @@ function Dasboard() {
                                 Create and edit topics adapted to your skill level.
                             </Card.Text>
                         </Card.Body>
+                        <Card.Footer>
+            <LinkContainer to="/MyTopics" className="clickableElement">
+
+                <button className="button-primary">Topics</button>
+            </LinkContainer>
+
+                </Card.Footer>
                     </Card>
-                      </LinkContainer>  
                 </Col>
             </Row>
         </Container>
